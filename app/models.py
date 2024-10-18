2c96ede92e07dcba0905bd7bea116499c4a933b3
Alex <4822007@student.isf.edu.hk>
2024-10-18 09:08:31 +0800

    #today I tested all of my MVPs (fields, methods, routes) in shell and in HTTPie. They all ran pretty smoothly, so I moved on to some other features for my API, such as check_complete and archive in fields, change_complete and change_archive in methods, and all/truth, all/dare, /search, /random, /reject, and /change_archive in views (routes). However I haven't tested all of them yet. #next I will finish all of the intended features on my planning document and try to think about what additional features I can add to my API to make it more engaging. Question: After finishing a round of game with my API, I would like to reset everything to normal for a second round of game. But I'm a bit stuck with how to change things such as change_complete and change_archive to False again. What can I do to modify that?

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class TOD(Model):
     statement = StringField()
     truth = BooleanField()
     dare = BooleanField()
+    check_complete = BooleanField()
+    archive = BooleanField()
 
     def json_response(self):
 
@@ -13,10 +15,20 @@ class TOD(Model):
             'id': self.id,
             'statement': self.statement,
             'truth': self.truth,
-            'dare': self.dare
+            'dare': self.dare,
+            'check_complete': self.check_complete,
+            'archive': self.archive
         }
 
     def change_statement(self, new_statement):
         self.statement = new_statement
+        self.save()
+
+    def change_complete(self):
+        self.check_complete = True
+        self.save()
+
+    def change_archive(self):
+        self.archive = True
         self.save()
         
--- app/views.py
+++ app/views.py
@@ -10,25 +10,96 @@ def new_tod(args):
         statement = args['statement'],
         truth = args['truth'],
         dare = args['dare'],
+        check_complete = False,
+        archive = False
     )
 
     new_tod.save()
 
-    return {'truth_or_dare': new_tod.json_response()}
+    return {'new_truth_or_dare': new_tod.json_response()}
 
 @route_get(BASE_URL + 'all')
 def all_tod(args):
-    # if TOD.objects.exists():
     tods_list = []
 
-    for tod in TOD.objects.all():
+    for tod in TOD.objects.filter(archive = False):
         tods_list.append(tod.json_response())
 
-    return {'truth_or_dare': tods_list}
+    return {'all_truths_or_dares': tods_list}
 
-    # else:
-    #     return{'error': 'No truth or dare exists'}
-
-# @route_get(BASE_URL + 'reject', args={'id':int, 'truth':bool, 'dare':bool})
-# def reject_tod(args):
+@route_get(BASE_URL + 'one', args={'id': int})
+def one_tod(args):
     
+    if TOD.objects.filter(id=args['id']).exists():
+        one_tod = TOD.objects.get(id=args['id'])
+
+        return {'one_truth_or_dare': one_tod.json_response()}
+
+    else:
+        return {'error': 'No truth or dare exists'}
+
+@route_get(BASE_URL + 'random')
+def random_tod(args):
+    
+    random_tod = TOD.objects.filter(archive = False).order_by('?').first()
+    return {'random_truth_or_dare': random_tod.json_response()}
+
+@route_post(BASE_URL + 'change_complete', args={'id':int})
+def complete_tod(args):
+    if TOD.objects.filter(id=args['id']).exists():
+        complete_tod = TOD.objects.get(id=args['id'])
+
+        complete_tod.change_complete()
+
+        return {'completed_truth_or_dare': complete_tod.json_response()}
+
+    else: 
+        return {'error': 'No truth or dare exists'}
+
+@route_get(BASE_URL + 'reject', args={'id':int})
+def reject_tod(args):
+    if TOD.objects.filter(id=args['id']).exists():
+        reject_tod = TOD.objects.filter(archive = False).exclude(id=args['id']).order_by('?').first()
+
+        return {'another_truth_or_dare': reject_tod.json_response()}
+
+    else:
+        return {'error': 'No truth or dare exists'}
+
+@route_post(BASE_URL + 'change_archive', args={'id':int})
+def archive_tod(args):
+    if TOD.objects.filter(id=args['id']).exists():
+        archive_tod = TOD.objects.get(id=args['id'])
+
+        archive_tod.change_archive()
+
+        return {'archived_tod': archive_tod.json_response()}
+
+    else: 
+        return {'error': 'No fortune exists'}
+
+@route_get(BASE_URL + 'all/truth')
+def all_truths(args):
+    truth_list = []
+
+    for tods in TOD.objects.filter(truth = True).filter(archive = False):
+        truth_list.append(tods.json_response())
+        
+    return {'all_truths': truth_list}
+
+@route_get(BASE_URL + 'all/dare')
+def all_dares(args):
+    dare_list = []
+
+    for tods in TOD.objects.filter(dare = True).filter(archive = False):
+        dare_list.append(tods.json_response())
+        
+    return {'all_dares': dare_list}
+
+@route_get(BASE_URL + 'search', args={'keyword':str})
+def search_tods(args):
+    search_list = []
+    for search in TOD.objects.filter(statement__contains = args['keyword']).filter(archive = False):
+        search_list.append(search.json_response())
+
+    return {'search_list': search_list}
